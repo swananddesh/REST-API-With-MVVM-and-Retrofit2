2aeea86519bb5186e7413650f7a53d5c074567a8
Swanand Deshpande <swananddesh15@gmail.com>
2019-09-04 23:12:17 +0530

    Base URL and API key added to use Food2Fork Recipe APIs.

---
+++ app/src/main/java/com/stalwart/foodrecipes/util/AppConstants.java
@@ -0,0 +1,9 @@
+package com.stalwart.foodrecipes.util;
+
+
+import com.stalwart.foodrecipes.BuildConfig;
+
+public class AppConstants {
+    public static final String BASE_URL = BuildConfig.BASE_URL;
+    public static final String API_KEY = BuildConfig.API_KEY;
+}
